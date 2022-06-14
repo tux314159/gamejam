840048fa6e168d8784cc3c3b27c4f47d5e1245e6
Isaac <tux314159@gmail.com>
2022-06-15 05:18:20 +0800

    lol

--- src/Game/Lib.hs
+++ src/Game/Lib.hs
@@ -6,13 +6,11 @@ import Control.Lens
 import Control.Monad
 import Control.Monad.State
 import GHC.Word
-import Game.PixelRender
 import Game.SdlUtils.Events
 import SDL hiding (get, set)
 
 data GameState = GameState
-  { _gamei :: Int,
-    _globalFrame :: Frame
+  { _gamei :: Int
   }
 
 makeLenses ''GameState
@@ -24,9 +22,6 @@ gameLoop renderer = do
   frame <- gets _globalFrame
   rendererDrawColor renderer $= V4 (fromIntegral i) 64 (255 - fromIntegral i) 255
   clear renderer
-
-  globalFrame %= framePutPixel (i `rem` 64, i `rem` 64) (64, 64, 64)
-  renderFrame frame renderer
 
   present renderer
   delay 15 -- should give <= 60 fps
---
+++ src/Game/Sprite/Sprite.hs
@@ -0,0 +1,6 @@
+module Game.Sprite.Sprite where
+
+import qualified Data.Vector as V
+import Codec.BMP
+
+data Sprite = Sprite { 
